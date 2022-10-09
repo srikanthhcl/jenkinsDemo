c3930a739f0279bda6ac991ab6ce8cead4f95e47
srikaanthhcl <srikanthb.testing@gmail.com>
2022-10-09 11:33:06 +0530

    Hello java file update

--- JenkinsWorkflow/src/pkgHello/Hello.java
+++ JenkinsWorkflow/src/pkgHello/Hello.java
@@ -1,12 +1,10 @@
-package pkgHello;
-
 public class Hello
 {
 	public static void main(String[] args)
 	{
 		for(int i=1;i<11;i++)
 		{
-			System.out.println("Hello World... "+i);
+			System.out.println("Hello World Executing from JenkinsS... "+i);
 		}
 	}
 
